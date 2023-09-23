d308325e9094cf13d9a9038299f17586ef57b1f2
tomas <tomasdc2016@gmail.com>
2023-09-23 16:06:34 -0300

    ambiente

---
+++ src/main/java/com/DIOSantander/BootcampDiOsantanderApplication.java
@@ -0,0 +1,13 @@
+package com.DIOSantander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampDiOsantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampDiOsantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DIOSantander/BootcampDiOsantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DIOSantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampDiOsantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
