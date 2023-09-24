8c3b9dd640438a11d606d66759a1cd3dce32cd28
tomas <tomasdc2016@gmail.com>
2023-09-24 14:00:40 -0300

    feat: Criado o repository para Transaction

---
+++ src/main/java/com/DIOSantander/repositories/TransactionRepository.java
@@ -0,0 +1,8 @@
+package com.DIOSantander.repositories;
+
+import com.DIOSantander.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
