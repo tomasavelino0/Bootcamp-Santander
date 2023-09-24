c4b33bb9edfa53a9be6c99d17d253bb1866b5d5a
tomas <tomasdc2016@gmail.com>
2023-09-24 18:20:43 -0300

    feat: Adicinado a documentacao SWAGGER e sua dependencia no pom.xml.

--- src/main/java/com/DIOSantander/BootcampDiOsantanderApplication.java
+++ src/main/java/com/DIOSantander/BootcampDiOsantanderApplication.java
@@ -1,13 +1,18 @@
 package com.DIOSantander;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title =
+    "API Desenvolvida como projeto final para o bootcamp SantanderDIO, "
+        + "você poderá criar usuários e simular uma transação financeira.", version = "2.0", description = "Employees Information"))
 public class BootcampDiOsantanderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootcampDiOsantanderApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BootcampDiOsantanderApplication.class, args);
+  }
 
 }
