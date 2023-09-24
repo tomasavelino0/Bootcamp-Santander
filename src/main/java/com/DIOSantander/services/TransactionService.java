80efa27e324932012fdf132521eb0b0bcc76ede5
tomas <tomasdc2016@gmail.com>
2023-09-24 14:03:13 -0300

    feat: adicionado os servicos de Transaction

---
+++ src/main/java/com/DIOSantander/services/TransactionService.java
@@ -0,0 +1,39 @@
+package com.DIOSantander.services;
+
+import com.DIOSantander.Dtos.TransactionDTO;
+import com.DIOSantander.domain.transaction.Transaction;
+import com.DIOSantander.domain.user.User;
+import com.DIOSantander.repositories.TransactionRepository;
+import java.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+
+  @Autowired
+  private UserService userService;
+
+  @Autowired
+  private TransactionRepository reposity;
+
+  public void createTransaction(TransactionDTO transactionDTO) throws Exception {
+    User sender = this.userService.findUserById(transactionDTO.senderId());
+    User receiver = this.userService.findUserById(transactionDTO.receiverId());
+
+    userService.validateTransaction(sender, transactionDTO.value());
+
+    Transaction transaction = new Transaction();
+    transaction.setAmount(transactionDTO.value());
+    transaction.setSender(sender);
+    transaction.setReceiver(receiver);
+    transaction.setTimestamp(LocalDateTime.now());
+
+    sender.setBalance(sender.getBalance().subtract(transactionDTO.value()));
+    receiver.setBalance(receiver.getBalance().add(transactionDTO.value()));
+
+    reposity.save(transaction);
+    userService.saveUser(sender);
+    userService.saveUser(receiver);
+  }
+}
