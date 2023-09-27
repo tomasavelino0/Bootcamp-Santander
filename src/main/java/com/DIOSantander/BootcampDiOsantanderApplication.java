c6b559b9660cfdbf0abc327b7481b4f37eee545e
tomas <tomasdc2016@gmail.com>
2023-09-27 14:04:58 -0300

    bugfix: CORS.

--- src/main/java/com/DIOSantander/BootcampDiOsantanderApplication.java
+++ src/main/java/com/DIOSantander/BootcampDiOsantanderApplication.java
@@ -2,11 +2,13 @@ package com.DIOSantander;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title =
+@OpenAPIDefinition(servers = {
+    @Server(url = "/", description = "default server URL")}, info = @Info(title =
     "API Desenvolvida como projeto final para o bootcamp SantanderDIO, "
         + "você poderá criar usuários e simular uma transação financeira.", version = "2.0", description = "transacao entre usuarios"))
 public class BootcampDiOsantanderApplication {
